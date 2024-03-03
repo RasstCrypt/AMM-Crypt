a1ee0b266a788962a7f22204aa0d7bfadb22128b
Chikal Anugrah <48489394+RasstCrypt@users.noreply.github.com>
2024-03-03 13:51:54 +0000

    Update adress and dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,30 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, coinbaseWallet, metamaskWallet, okxWallet } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { BerachainArtio } from "@thirdweb-dev/chains";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
+import { metamaskWallet, coinbaseWallet, okxWallet } from "@thirdweb-dev/react";
+import darkTheme from "../styles/theme";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-//const activeChain = "Berachain";
-//const apikey = "265def4768f24970299dba3991993362";
+const activeChain = BerachainArtio;
+
+const customTheme = extendTheme({
+  ...darkTheme,
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-    supportedWallets={[
-      metamaskWallet({
-        recommended:true,
-      }),
-      coinbaseWallet(),
-      okxWallet(),
-    ]}
-      clientId="265def4768f24970299dba3991993362"
-      activeChain={BerachainArtio}
+      supportedWallets={[metamaskWallet({ recommended: true }), coinbaseWallet({}), okxWallet({})]}
+      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      activeChain={activeChain}
     >
-      <ChakraProvider>
-        <NavBar/>
+      <ChakraProvider theme={customTheme}>
+        <NavBar />
         <Component {...pageProps} />
       </ChakraProvider>
     </ThirdwebProvider>
